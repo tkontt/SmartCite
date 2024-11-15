3d07570570691a55b23835ca4232ce0a58903dc1
niko <niko.summanen1@gmail.com>
2024-11-15 13:59:05 +0200

    Tietokanta setup

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,48 +1,69 @@
-from config import db, app
 from sqlalchemy import text
+from config import db, app
 
-table_name = "todos"
+CITATIONS_TABLE = "citations"
+CITATION_FIELDS_TABLE = "citation_fields"
 
 def table_exists(name):
-  sql_table_existence = text(
-    "SELECT EXISTS ("
-    "  SELECT 1"
-    "  FROM information_schema.tables"
-    f" WHERE table_name = '{name}'"
-    ")"
-  )
+    sql_table_existence = text(
+        "SELECT EXISTS ("
+        "    SELECT 1"
+        "    FROM information_schema.tables"
+        f"   WHERE table_name = '{name}'"
+        ")"
+    )
 
-  print(f"Checking if table {name} exists")
-  print(sql_table_existence)
+    print(f"Checking if table {name} exists")
+    print(sql_table_existence)
 
-  result = db.session.execute(sql_table_existence)
-  return result.fetchall()[0][0]
+    result = db.session.execute(sql_table_existence)
+    return result.fetchall()[0][0]
 
 def reset_db():
-  print(f"Clearing contents from table {table_name}")
-  sql = text(f"DELETE FROM {table_name}")
-  db.session.execute(sql)
-  db.session.commit()
+    clear_table(CITATIONS_TABLE)
+    clear_table(CITATION_FIELDS_TABLE)
 
-def setup_db():
-  if table_exists(table_name):
-    print(f"Table {table_name} exists, dropping")
-    sql = text(f"DROP TABLE {table_name}")
+def clear_table(table_name: str):
+    print(f"Clearing contents from table {table_name}")
+    sql = text(f"DELETE FROM {table_name}")
     db.session.execute(sql)
     db.session.commit()
 
-  print(f"Creating table {table_name}")
-  sql = text(
-    f"CREATE TABLE {table_name} ("
-    "  id SERIAL PRIMARY KEY, "
-    "  content TEXT NOT NULL,"
-    "  done BOOLEAN DEFAULT FALSE"
-    ")"
-  )
+def setup_db():
+    drop_table_if_exists(CITATIONS_TABLE)
+    drop_table_if_exists(CITATION_FIELDS_TABLE)
 
-  db.session.execute(sql)
-  db.session.commit()
+    print(f"Creating table {CITATIONS_TABLE}")
+    sql = text(
+        f"CREATE TABLE {CITATIONS_TABLE} ("
+        "    id SERIAL PRIMARY KEY, "
+        "    citation_type TEXT NOT NULL,"
+        "    citation_key TEXT NOT NULL UNIQUE"
+        ")"
+    )
+    db.session.execute(sql)
+
+    print(f"Creating table {CITATION_FIELDS_TABLE}")
+    sql = text(
+        f"CREATE TABLE {CITATION_FIELDS_TABLE} ("
+        "    id SERIAL PRIMARY KEY, "
+        f"   citation_id INT REFERENCES {CITATIONS_TABLE} (id) ON DELETE CASCADE,"
+        "    field_name TEXT NOT NULL,"
+        "    field_value TEXT NOT NULL,"
+        "    UNIQUE (citation_id, field_name)"
+        ")"
+    )
+    db.session.execute(sql)
+
+    db.session.commit()
+
+def drop_table_if_exists(table_name: str):
+    if table_exists(table_name):
+        print(f"Table {table_name} exists, dropping")
+        sql = text(f"DROP TABLE {table_name} CASCADE")
+        db.session.execute(sql)
+        db.session.commit()
 
 if __name__ == "__main__":
     with app.app_context():
-      setup_db()
+        setup_db()

95a87d031687d0d168a527e908f99af6c14f6aae
Hanna Turunen <hannamatilda.turunen@gmail.com>
2024-11-17 11:08:00 +0200

    Alustava lomake ja kotinäkymä

--- src/app.py
+++ src/app.py
@@ -1,30 +1,36 @@
 from flask import redirect, render_template, request, jsonify, flash
 from db_helper import reset_db
 from repositories.todo_repository import get_todos, create_todo, set_done
+from repositories.citation_repository import get_citations
 from config import app, test_env
 from util import validate_todo
 
 @app.route("/")
 def index():
-    todos = get_todos()
-    unfinished = len([todo for todo in todos if not todo.done])
-    return render_template("index.html", todos=todos, unfinished=unfinished) 
+    citations = get_citations()
+    return render_template("index.html", citations=citations) 
 
-@app.route("/new_todo")
+@app.route("/new_citation")
 def new():
-    return render_template("new_todo.html")
+    return render_template("new_citation.html")
 
-@app.route("/create_todo", methods=["POST"])
-def todo_creation():
+@app.route("/create_citation", methods=["POST"])
+def citation_creation():
+    citation_type = request.form.get("citation_type")
     content = request.form.get("content")
+    author = request.form.get("author/s")
 
     try:
+        # tämä ei vielä tehty
+        # create_citation(citation_type, author, content)
+        
         validate_todo(content)
         create_todo(content)
+
         return redirect("/")
     except Exception as error:
         flash(str(error))
-        return  redirect("/new_todo")
+        return  redirect("/new_citation")
 
 @app.route("/toggle_todo/<todo_id>", methods=["POST"])
 def toggle_todo(todo_id):

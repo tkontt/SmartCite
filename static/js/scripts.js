3c0d5f32bc9d1c2ca2b870b2210b3657ba5ed9e5
Tuomas Konttinen <konttinen.tuomas@gmail.com>
2024-11-21 03:12:47 +0200

    hakukenttä ja latex quick copy

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -13,3 +13,33 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });    
+// Kopioi latex key clipboardille
+function copyToClipboard(text) {
+        const tempInput = document.createElement("textarea");
+        tempInput.value = text;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        try {
+            document.execCommand("copy");
+            alert("Copied to clipboard: " + text);
+        } catch (err) {
+            alert("Failed to copy text. Please try again.");
+        }
+        document.body.removeChild(tempInput);
+}
+// Hakutoiminno Javascript
+function filterTable() {
+    const input = document.getElementById("search-input");
+    const filter = input.value.toLowerCase();
+    const table = document.getElementById("citations-table");
+    const rows = table.getElementsByTagName("tr");
+
+    for (let i = 1; i < rows.length; i++) { // Aloita riviltä 1, skippaa header row
+        const cells = rows[i].getElementsByTagName("td");
+        let rowText = "";
+        for (let j = 0; j < cells.length; j++) {
+            rowText += cells[j].textContent.toLowerCase();
+        }
+        rows[i].style.display = rowText.includes(filter) ? "" : "none";
+    }
+}

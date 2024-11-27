48e338f0b4f95d0d9ade52def843013d1b98244f
niko <niko.summanen1@gmail.com>
2024-11-27 12:57:03 +0200

    Bibtex unit test

---
+++ src/tests/bibtex_generation_test.py
@@ -0,0 +1,44 @@
+import unittest
+from entities.citation import Citation
+from util import generate_bibtex
+
+class TestBibtexGeneration(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_bibtex_generates_correctly(self):
+        citations = []
+        citations.append(Citation("article", "1",
+        {
+            "author": "1",
+            "title": "1",
+            "year": "1",
+            "journal": "1"
+        }))
+        citations.append(Citation("article", "2",
+        {
+            "author": "2",
+            "title": "2",
+            "year": "2",
+            "journal": "2"
+        }))
+
+        bibtex = generate_bibtex(citations)
+
+        expected = """@article{1,
+\tauthor = {1},
+\ttitle = {1},
+\tyear = {1},
+\tjournal = {1},
+}
+
+@article{2,
+\tauthor = {2},
+\ttitle = {2},
+\tyear = {2},
+\tjournal = {2},
+}
+
+"""
+
+        self.assertEqual(bibtex, expected)
